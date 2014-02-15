8c21b7470585410cb61af5db67ea1ea37f8bff20
viperscape <chrisfgill@gmail.com>
2014-02-15 10:09:16 -0500

    Update project.clj

--- project.clj
+++ project.clj
@@ -20,6 +20,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
+  [org.clojure/core.async "0.1.242.0-44b1e3-alpha"]
  		             [org.clojure/data.xml "0.0.7"]
                  [org.clojure/data.zip "0.1.1"]
                  [org.lwjgl.lwjgl/lwjgl "2.8.5"]
@@ -30,5 +31,4 @@
                   [net.mikera/core.matrix "0.11.0"]
                   [org.clojure/tools.nrepl "0.2.3"]]
   :main honuscape.core)
-
 
